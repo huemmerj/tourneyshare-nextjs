227b814b2824d7ba5b51c7327880ec56d7528ba9
Jens <huemmer.jens@gmail.com>
2025-10-14 14:22:56 +0200

    feat: implement get all tournaments API endpoint and update TournamentsOverview component to fetch tournaments

--- app/app/api/tournaments/route.ts
+++ app/app/api/tournaments/route.ts
@@ -1,22 +1,41 @@
 // create get all tournaments route using firebase admin sdk
 import { NextRequest, NextResponse } from "next/server";
 import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 import { app as firebaseAdminApp } from "@/lib/firebase-admin";
 
 export async function GET(request: NextRequest) {
   try {
     const auth = getAuth(firebaseAdminApp);
-    // get all users (up to 1000)
-    const listUsersResult = await auth.listUsers(1000);
-    const users = listUsersResult.users.map((userRecord) => ({
-      uid: userRecord.uid,
-      email: userRecord.email,
-      displayName: userRecord.displayName,
-    }));
+    try {
+      // 2. Get a reference to the Firestore database.
+      const db = getFirestore();
+      const tournamentsRef = db.collection("tournaments");
+      const snapshot = await tournamentsRef.get();
 
-    return NextResponse.json({ users });
+      // 3. Check if the collection is empty.
+      if (snapshot.empty) {
+        console.info("No tournaments found.");
+        return NextResponse.json({ tournaments: [] });
+      }
+
+      // 4. Map the documents to an array of data.
+      const tournaments = snapshot.docs.map(
+        (doc: { id: any; data: () => any }) => ({
+          id: doc.id,
+          ...doc.data(),
+        })
+      );
+
+      return NextResponse.json({ tournaments });
+    } catch (error) {
+      console.error("Error fetching tournaments:", error);
+      throw NextResponse.json(
+        { error: "An error occurred while fetching tournaments." },
+        { status: 500 }
+      );
+    }
   } catch (error) {
-    console.error("Error fetching users:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
--- app/app/tournaments/components/tournaments-overview.tsx
+++ app/app/tournaments/components/tournaments-overview.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 export async function TournamentsOverview() {
   const getTournaments = async () => {
-    const response = await fetch("/api/tournaments");
+    // fetch from /api/tournaments
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_APP_URL}/api/tournaments`
+    );
     return response.json();
   };
   const tournaments = await getTournaments();
 
+  console.log(tournaments);
   return <div>Tournaments Overview</div>;
 }
